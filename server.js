b50961c834a5b4aac913d43b2275651cd8e4190e
Mateie <matemachitidze2001@gmail.com>
2022-12-07 11:41:57 -0800

    updated server

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
-let server = process.env.NODE_ENV !== 'development' ? require('https').createServer({
-    key: fs.readFileSync(__dirname + '/private.key', 'utf-8'),
-    cert: fs.readFileSync(__dirname + '/public.cer', 'utf-8'),
-    ca: fs.readFileSync(__dirname + '/ca.cer', 'utf-8')
-}, app) : require('http').createServer(app);
+let server = process.env.NODE_ENV !== "development" ? require("https").createServer({
+    key: fs.readFileSync(__dirname + "/private.key", "utf-8"),
+    cert: fs.readFileSync(__dirname + "/public.cer", "utf-8"),
+    ca: fs.readFileSync(__dirname + "/ca.cer", "utf-8")
+}, app) : require("http").createServer(app);
 
-const port = process.env.NODE_ENV === 'development' ? 80 : process.env.PORT;
+const port = process.env.NODE_ENV === "development" ? 80 : process.env.PORT;
 const publicPath = path.join(__dirname, "build");
 
 app.use(express.static(publicPath));
